78f19ddffe925dc956af348fcb566cf04eae356a
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-04-01 09:38:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function changeIntoUpper(str){
 let store=""
 	for(let i of str){
-		if(str[i]==='a' && str[i]==='z' || str[i]==="A" && str[i]==='Z'){
+		if(str[i]>='a' && str[i]<='z' || str[i]>="A" && str[i]<='Z'){
 			store=store+str[i]
 			store=store.toUpperCase()
 		}
